e1f45b8c65897748ebbe1ccb7200013d0959140c
zaemyung-test <kim01756@umn.edu>
2026-02-14 00:58:35 -0800

    Create dev-alex branch and add placeholder package for ModelHub

---
+++ src/modelhub/__init__.py
@@ -0,0 +1,9 @@
+"""ModelHub package placeholder.
+
+This file is a minimal placeholder to initialize the package on the dev-alex branch.
+"""
+
+__version__ = "0.1.0"
+
+def hello():
+    return "ModelHub placeholder from dev-alex"
